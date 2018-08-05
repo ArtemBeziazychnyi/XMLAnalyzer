e4bbf7f0bed2ed15ea196c779f95718df0008bc2
Artem Beziazychnyi <artembeziazychnyi@gmail.com>
2018-08-05 17:06:53 +0300

    Initial commit

---
+++ XMLAnalyzer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace XMLAnalyzer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				Console.WriteLine("Please input path to the file");
+				return;
+			}
+
+			var xmlAnalyzer = new XMLAnalyzer();
+
+			Dictionary<string, string> attributes;
+
+			if (File.Exists(args.First()))
+			{
+				attributes = xmlAnalyzer.GetTargetAttributes(args[0]);
+			}
+			else
+			{
+				Console.WriteLine($"Original file '{Path.GetFileName(args.First())}' not found");
+				return;
+			}
+
+			foreach (var htmlFilePath in args)
+			{
+				if (!File.Exists(htmlFilePath))
+				{
+					Console.WriteLine($"File '{Path.GetFileName(htmlFilePath)}' not found");
+					continue;
+				}
+
+				var xPatch = xmlAnalyzer.GetXPathToElement(htmlFilePath, attributes);
+				Console.WriteLine($"XPath to the target element: {xPatch}");
+			}
+
+			Console.WriteLine("Press any key to exit from the programm");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ XMLAnalyzer/XMLAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ScrapySharp.Extensions;
+
+namespace XMLAnalyzer
+{
+	public class XMLAnalyzer
+	{
+		private readonly HtmlAgilityPack.HtmlDocument _document = new HtmlAgilityPack.HtmlDocument();
+
+		public string GetXPathToElement(string filePatch, Dictionary<string, string> attributes)
+		{
+			_document.Load(filePatch);
+
+			var element = _document.DocumentNode.CssSelect("a")
+								.FirstOrDefault(el => el.Attributes.Contains("rel") && el.Attributes.Contains("title")
+													  &&
+													  (el.Attributes["rel"].Value == attributes["rel"] ||
+													   el.Attributes["title"].Value == attributes["title"]));
+
+
+			if (element == null)
+				throw new Exception("Not found target element");
+
+			return element.XPath;
+		}
+
+		public Dictionary<string, string> GetTargetAttributes(string originalFilePatch)
+		{
+			var targetElementId = "make-everything-ok-button";
+
+			_document.Load(originalFilePatch);
+
+			var tag = _document.GetElementbyId(targetElementId);
+
+			return new Dictionary<string, string>
+			{
+				{"title", tag.Attributes["title"].Value},
+				{"rel", tag.Attributes["rel"].Value}
+			};
+		}
+	}
+}
+
